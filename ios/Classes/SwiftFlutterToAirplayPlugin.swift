1a1adb849efb0978f4ce02f71913ee95ffebcc4f
Gabriel Reis <gabriel.reis@greenn.com.br>
2025-03-20 06:34:53 -0300

    TEste

--- ios/Classes/SwiftFlutterToAirplayPlugin.swift
+++ ios/Classes/SwiftFlutterToAirplayPlugin.swift
@@ -1,8 +1,17 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class SwiftFlutterToAirplayPlugin: NSObject, FlutterPlugin {
+  private var routeDetector: AVRouteDetector?
+  private var channel: FlutterMethodChannel?
+  
   public static func register(with registrar: FlutterPluginRegistrar) {
+    let channel = FlutterMethodChannel(name: "flutter_to_airplay", binaryMessenger: registrar.messenger())
+    let instance = SwiftFlutterToAirplayPlugin()
+    instance.channel = channel
+    registrar.addMethodCallDelegate(instance, channel: channel)
+    
     registrar.register(
         SharePlatformViewFactory(messenger: registrar.messenger()),
         withId: "airplay_route_picker_view",
@@ -12,5 +21,52 @@ public class SwiftFlutterToAirplayPlugin: NSObject, FlutterPlugin {
         SharePlatformViewFactory(messenger: registrar.messenger()),
         withId: "flutter_avplayer_view",
         gestureRecognizersBlockingPolicy: FlutterPlatformViewGestureRecognizersBlockingPolicy(rawValue: 0))
+  }
+  
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "startMonitoringAirplayConnection":
+      startMonitoringAirplayConnection()
+      result(nil)
+    case "stopMonitoringAirplayConnection":
+      stopMonitoringAirplayConnection()
+      result(nil)
+    case "isConnectedToAirplay":
+      result(isConnectedToAirplay())
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+  
+  private func startMonitoringAirplayConnection() {
+    routeDetector = AVRouteDetector()
+    routeDetector?.routeDetectionEnabled = true
+    
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(handleRouteChange),
+      name: AVAudioSession.routeChangeNotification,
+      object: nil
+    )
+  }
+  
+  private func stopMonitoringAirplayConnection() {
+    routeDetector?.routeDetectionEnabled = false
+    routeDetector = nil
+    NotificationCenter.default.removeObserver(self, name: AVAudioSession.routeChangeNotification, object: nil)
+  }
+  
+  @objc private func handleRouteChange(notification: Notification) {
+    channel?.invokeMethod("onAirplayConnectionChanged", arguments: ["connected": isConnectedToAirplay()])
+  }
+  
+  private func isConnectedToAirplay() -> Bool {
+    let currentRoute = AVAudioSession.sharedInstance().currentRoute
+    for output in currentRoute.outputs {
+      if output.portType == AVAudioSession.Port.airPlay {
+        return true
+      }
+    }
+    return false
   }
 }
---
+++ lib/src/air_play_connection_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+
+class AirplayConnectionManager {
+  static const MethodChannel _channel = MethodChannel('flutter_to_airplay');
+
+  static Future<void> startMonitoring(
+      {Function(bool)? onConnectionChanged}) async {
+    _channel.setMethodCallHandler((call) async {
+      if (call.method == 'onAirplayConnectionChanged') {
+        final args = call.arguments as Map;
+        final connected = args['connected'] as bool;
+        onConnectionChanged?.call(connected);
+      }
+    });
+
+    await _channel.invokeMethod('startMonitoringAirplayConnection');
+  }
+
+  static Future<void> stopMonitoring() async {
+    await _channel.invokeMethod('stopMonitoringAirplayConnection');
+  }
+
+  static Future<bool> isConnectedToAirplay() async {
+    return await _channel.invokeMethod('isConnectedToAirplay');
+  }
+}
